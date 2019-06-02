6d7ca04aa38cd67329af83dd00597d4e9b21a5ef
Mermaz <m.dessiehi@hotmail.fr>
2019-06-02 18:32:57 +0200

    FizzBuzz : Création de la solution et la fonctionnalité du multiple de trois

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static string MultipleDeTrois(int nombre)
+        {
+            if ((nombre % 3) == 0)
+                return "Fizz";
+            return string.Empty;
+        }
+
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                string s = string.Empty;
+                s = MultipleDeTrois(i);
+                Console.WriteLine(string.Format("Nombre : {0}, valeur : {1}",i,s));
+            }
+            Console.ReadKey();
+        }
+    }
+}
