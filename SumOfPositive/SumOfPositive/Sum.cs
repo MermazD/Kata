c54293a98308f0c42ce83f4330b5f80e67238d70
Mermaz <m.dessiehi@hotmail.fr>
2019-06-12 23:22:08 +0200

    Sum Of Positive : ajout de la solution

---
+++ SumOfPositive/SumOfPositive/Sum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfPositive
+{
+    public class Sum
+    {
+        public static int PositiveSum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                    sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
